010a17a154ec21e1e8efc90b628d5b19d5bfcfe0
Dan Friedman <dan.friedman@logicpd.com>
2016-01-06 20:08:07 -0600

    Initial checkin

---
+++ OneDrive2Rss/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Text;
+using System.Xml;
+
+namespace OneDrive2Rss
+{
+    class Program
+    {
+        private static readonly string UrlFormatString = "https://api.onedrive.com/v1.0/drives/{0}/root:/public/podcast:/children";
+        private static readonly string AudioUrlFormatString = "https://api.onedrive.com/v1.0/drives/{0}/root:/public/podcast/{1}:/content";
+
+        static void Main(string[] args)
+        {
+            var feedTitle = ConfigurationManager.AppSettings["FeedTitle"];
+            var description = ConfigurationManager.AppSettings["FeedDescription"];
+            var deviceId = ConfigurationManager.AppSettings["OneDriveDeviceId"];
+
+            var feedLink = string.Format(UrlFormatString, deviceId);
+
+            var directoryPath = args[0];
+            var di = new DirectoryInfo(directoryPath);
+            var syndicationItems = di.EnumerateFiles("*.mp3").Select(f =>
+                new SyndicationItem(f.Name, SyndicationContent.CreateUrlContent(new Uri(string.Format(AudioUrlFormatString, deviceId, f.Name)), "application/mp3"), null, null, f.CreationTime)
+);
+            var feed = new SyndicationFeed(feedTitle, description, new Uri(feedLink), syndicationItems);
+
+            using (var textWriter = new XmlTextWriter(Path.Combine(directoryPath, "rss.xml"), Encoding.UTF8))
+            {
+                feed.SaveAsRss20(textWriter);
+            }
+        }
+    }
+}
